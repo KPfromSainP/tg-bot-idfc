540b2cba3aab2eba8ad6464139dcf1ac8e2bf080
Kirill Podkovyrin <kyrimer2010@gmail.com>
2025-03-17 12:13:01 +0300

    init

---
+++ src/main/java/com/kirill/tgbotidfc/TgBotIdfcApplication.java
@@ -0,0 +1,13 @@
+package com.kirill.tgbotidfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotIdfcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgBotIdfcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kirill/tgbotidfc/TgBotIdfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirill.tgbotidfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotIdfcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
